720fcb66320a67acf388b8cb3cd485f27672d668
davi3234 <davi.fadriano@gmail.com>
2024-11-08 14:36:28 -0300

    feat: Creating ApiException

---
+++ core/Exceptions/ApiException.php
@@ -0,0 +1,18 @@
+<?php
+namespace Core\Exceptions;
+
+use Core\Enums\HttpStatus;
+use Exception;
+
+class ApiException extends Exception {
+    private HttpStatus $status;
+
+    public function __construct(HttpStatus $status, string $message = '') {
+        parent::__construct($message);
+        $this->status = $status;
+    }
+
+    public function getStatus(): HttpStatus {
+        return $this->status;
+    }
+}
