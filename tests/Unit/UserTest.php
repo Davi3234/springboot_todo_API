ac9e72ebe21be8c4e9ea6e5e55a7511aea4286ab
davi3234 <davi.fadriano@gmail.com>
2024-12-18 16:44:31 -0300

    feat: Adding test to throw exception for invalid name

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -6,42 +6,76 @@ use App\DTOs\UserDTO;
 use App\Models\User;
 use App\Repositories\IUserRepository;
 use App\Services\UserService;
+use Core\Exceptions\ValidationException;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-  class UserTest extends TestCase{
+class UserTest extends TestCase
+{
 
-    #[Test]
-    public function mustCreateUser()
-    {
-        // Arrange
-        $dto = new UserDTO(
-            id: 1,
-            name: "Davi",
-            email: "davi@gmail.com",
-            password: "davi1234"
-        );
+  #[Test]
+  public function mustCreateUser(){
+    // Arrange
+    $dto = new UserDTO(
+      id: 1,
+      name: "Davi",
+      email: "davi@gmail.com",
+      password: "davi1234"
+    );
 
-        $mockUser = new User(
-            id: 1,
-            name: $dto->name,
-            email: $dto->email,
-            password: $dto->password
-        );
+    $mockUser = new User(
+      id: 1,
+      name: $dto->name,
+      email: $dto->email,
+      password: $dto->password
+    );
 
-        $userRepository = $this->createMock(IUserRepository::class);
+    $userRepository = $this->createStub(IUserRepository::class);
 
-        $userRepository
-            ->method('create')
-            ->with($dto)
-            ->willReturn($mockUser);
+    $userRepository
+      ->method('create')
+      ->with($dto)
+      ->willReturn($mockUser);
 
-        $userService = new UserService($userRepository);
+    $userService = new UserService($userRepository);
 
-        // Act
-        $result = $userService->create($dto);
+    // Act
+    $result = $userService->create($dto);
 
-        // Assert
-        $this->assertEquals($mockUser, $result);
-    }
+    // Assert
+    $this->assertEquals($mockUser, $result);
   }
+
+  #[Test]
+  public function mustThrowExceptToInvalidName(){
+    $this->expectException(ValidationException::class);
+
+    // Arrange
+    $dto = new UserDTO(
+      id: 1,
+      name: "",
+      email: "davi@gmail.com",
+      password: "davi1234"
+    );
+
+    $mockUser = new User(
+      id: 1,
+      name: $dto->name,
+      email: $dto->email,
+      password: $dto->password
+    );
+
+    $userRepository = $this->createStub(IUserRepository::class);
+
+    $userRepository
+      ->method('create')
+      ->with($dto)
+      ->willReturn($mockUser);
+
+    $userService = new UserService($userRepository);
+
+    // Act
+    // Must throw exception 
+    $result = $userService->create($dto);
+  }
+}
