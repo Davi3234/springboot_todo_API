8672b642e4c900d7b7ba9d787ccd26f93147d811
davi3234 <davi.fadriano@gmail.com>
2024-12-23 16:25:13 -0300

    fix: Changing to static method

--- src/main/java/com/project/todo_api/mappers/UserMapper.java
+++ src/main/java/com/project/todo_api/mappers/UserMapper.java
@@ -4,10 +4,10 @@ import com.project.todo_api.dtos.UserDTO;
 import com.project.todo_api.models.User;
 
 public class UserMapper {
-  public User toEntity(UserDTO userDto){
+  public static User toEntity(UserDTO userDto){
     return new User(userDto.getName(), userDto.getEmail(), userDto.getPassword());
   }
-  public UserDTO toDTO(User user){
+  public static UserDTO toDTO(User user){
     return new UserDTO(user.getId(),user.getName(), user.getEmail(), user.getPassword());
   }
 }
