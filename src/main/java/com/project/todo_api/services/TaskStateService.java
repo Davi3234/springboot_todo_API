3edf37f5ef40296e2aa319b1f7bd7db5d80d0894
davi3234 <davi.fadriano@gmail.com>
2025-01-08 13:57:52 -0300

    feat: Creating changeState function in task state service

--- src/main/java/com/project/todo_api/services/TaskStateService.java
+++ src/main/java/com/project/todo_api/services/TaskStateService.java
@@ -1,17 +1,33 @@
 package com.project.todo_api.services;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.todo_api.models.Task;
+import com.project.todo_api.repositories.TaskRepository;
+import com.project.todo_api.factories.TaskStateFactory;
+import com.project.todo_api.state.TaskState;
 
 @Service
 public class TaskStateService {
 
-  public void advanceToNextState(Task task) throws Exception {
-    task.getState().next(task);
-  }
+  @Autowired
+  private TaskRepository taskRepository;
 
-  public void revertToPreviousState(Task task) throws Exception {
-    task.getState().previous(task);
+  @Autowired
+  private TaskStateFactory taskFactory;
+
+  public Task changeState(Long taskId, String state) throws Exception {
+    Task task = taskRepository.findById(taskId)
+        .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+
+    TaskState newState = taskFactory.getState(state);
+
+    newState.applyState(task);
+
+    taskRepository.save(task);
+
+    return task;
   }
 }
