7e40995d9fd2c1c8a1c3c08a86c7b93a004bf7f1
davi3234 <davi.fadriano@gmail.com>
2025-01-08 08:25:16 -0300

    feat: Creating task controller

---
+++ src/main/java/com/project/todo_api/controllers/TaskController.java
@@ -0,0 +1,73 @@
+package com.project.todo_api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.todo_api.dtos.TaskDTO;
+import com.project.todo_api.models.Task;
+import com.project.todo_api.services.TaskService;
+import com.project.todo_api.services.TaskStateService;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+  @Autowired
+  private TaskService taskService;
+  @Autowired
+  private TaskStateService taskStateService;
+
+  @GetMapping
+  public ResponseEntity<List<Task>> getAllTasks() {
+    return ResponseEntity.ok(taskService.getAllTasks());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+    return taskService.getTaskById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  public ResponseEntity<Task> createTask(@RequestBody TaskDTO taskDto) {
+    return ResponseEntity.ok(taskService.createTask(taskDto));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDto) {
+    return ResponseEntity.ok(taskService.updateTask(id, taskDto));
+  }
+
+  @PutMapping("/next/{id}")
+  public ResponseEntity<Task> toNextState(@PathVariable Long id) {
+    try {
+      Task task = taskService.getTaskById(id)
+          .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+      taskStateService.advanceToNextState(task);
+      return ResponseEntity.ok(task);
+      
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.notFound().build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    taskService.deleteTask(id);
+    return ResponseEntity.noContent().build();
+  }
+}
