b4efbcc54633586a40b723735fb92bca698d69d7
davi3234 <davi.fadriano@gmail.com>
2025-01-08 08:24:39 -0300

    feat: Adding TaskState and changing enum names

--- src/main/java/com/project/todo_api/models/Task.java
+++ src/main/java/com/project/todo_api/models/Task.java
@@ -2,8 +2,9 @@ package com.project.todo_api.models;
 
 import java.util.Date;
 
-import com.project.todo_api.enums.Priority;
-import com.project.todo_api.enums.Status;
+import com.project.todo_api.enums.TaskPriority;
+import com.project.todo_api.enums.TaskStatus;
+import com.project.todo_api.state.TaskState;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,20 +23,26 @@ public class Task {
 
   private String title;
   private String description;
-  private Status status;
-  private Priority priority;
+  private TaskStatus status;
+  private TaskPriority priority;
   private Date creationDate;
   private Date endDate;
 
+  private TaskState state;
+
   public Task() {
+    this.status = TaskStatus.TODO;
+    this.state = this.status.toTaskState();
   }
 
-  public Task(String title, String description, Status status, Priority priority, Date creationDate, Date endDate) {
+  public Task(String title, String description, TaskStatus status, TaskPriority priority, Date creationDate,
+      Date endDate) {
     this.title = title;
     this.description = description;
     this.status = status;
     this.priority = priority;
     this.creationDate = creationDate;
     this.endDate = endDate;
+    this.state = this.status.toTaskState();
   }
 }
